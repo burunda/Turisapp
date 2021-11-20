fa73c76a6b5af4286882fec1f3c7a07b15915f51
Karol-Moncaleano <karolvilla03@gmail.com>
2021-11-20 14:53:16 -0500

    Desarrollo Recyclerview y Cardview

---
+++ app/src/main/java/com/example/turistappdescubrenuevoscaminos/CustomAdapter.java
@@ -0,0 +1,6 @@
+package com.example.turistappdescubrenuevoscaminos;
+
+import androidx.recyclerview.widget.RecyclerView
+
+class CustomeAdapter(private val mList: List<RecyclerviewModel>): RecyclerView.Adapter {
+}
---
+++ app/src/main/java/com/example/turistappdescubrenuevoscaminos/RecyclerviewActivity.kt
@@ -0,0 +1,4 @@
+package com.example.turistappdescubrenuevoscaminos
+
+data class RecyclerviewActivity(val image:Int,val text:String) {
+}
